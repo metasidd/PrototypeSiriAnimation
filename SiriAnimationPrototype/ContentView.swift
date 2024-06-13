05f2c72e50c09d310b3b13669671b867e1c981f8
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-06-13 22:27:44 +0530

    Moved from padding to scale

--- SiriAnimationPrototype/ContentView.swift
+++ SiriAnimationPrototype/ContentView.swift
@@ -11,49 +11,44 @@ struct ContentView: View {
     enum SiriState {
         case none
         case thinking
-        case answered
     }
     
     @State var state: SiriState = .none
     @State var timer: Timer?
     @State private var maskTimer: Float = 0.0
     
-    var computedPadding: CGFloat {
+    private var computedScale: CGFloat {
         switch state {
-        case .none: 0
-        case .thinking: 12
-        case .answered: 4
+        case .none: 1.1
+        case .thinking: 1
         }
     }
     
-    var rectangleSpeed: Float {
+    private var rectangleSpeed: Float {
         switch state {
         case .none: 0
         case .thinking: 0.1
-        case .answered: 0.02
         }
     }
     
-    var gradientSpeed: Float {
+    private var gradientSpeed: Float {
         switch state {
         case .none: 0
         case .thinking: 0.05
-        case .answered: 0.01
         }
     }
     
-    var animatedMaskBlur: CGFloat {
+    private var animatedMaskBlur: CGFloat {
         switch state {
         case .none: 8
         case .thinking: 24
-        case .answered: 16
         }
     }
     
-    var containerPpacity: CGFloat {
+    private var containerOpacity: CGFloat {
         switch state {
         case .none: 0
-        case .thinking, .answered: 1.0
+        case .thinking: 1.0
         }
     }
 
@@ -62,11 +57,13 @@ struct ContentView: View {
             ZStack {
                 meshGradient
                     .scaleEffect(1.5) // avoids masks clipping
-                    .opacity(containerPpacity)
+                    .opacity(containerOpacity)
                 
                 phoneBackground
                     .mask {
-                        animatedRectanle(proxy: geometry, t: CGFloat(maskTimer))
+                        AnimatedRectangle(size: geometry.size, cornerRadius: 48, t: CGFloat(maskTimer))
+                            .scaleEffect(computedScale)
+                            .frame(width: geometry.size.width, height: geometry.size.height)
                             .blur(radius: animatedMaskBlur)
                     }
             }
@@ -74,15 +71,14 @@ struct ContentView: View {
         .ignoresSafeArea()
     }
     
-    private func animatedRectanle(proxy: GeometryProxy, t: CGFloat) -> some View {
-        AnimatedRectangle(size: proxy.size, padding: computedPadding, cornerRadius: 48, t: t)
-            .frame(width: proxy.size.width, height: proxy.size.height)
-    }
-    
     private var phoneBackground: some View {
         VStack {
-            siriButton(text: "Start", action: { self.state = .thinking })
-            siriButton(text: "Answered", action: { self.state = .answered })
+            siriButton(text: "Start", action: {
+                    self.state = .thinking
+            })
+            siriButton(text: "Stop", action: {
+                self.state = .none
+            })
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         .background(Color.white)
@@ -90,7 +86,7 @@ struct ContentView: View {
     
     private func siriButton(text: String, action: @escaping () -> Void) -> some View {
         Button {
-            withAnimation(.easeInOut(duration: 1)) {
+            withAnimation(.smooth(duration: 0.9)) {
                 action()
             }
         } label: {
@@ -116,7 +112,7 @@ struct ContentView: View {
             [sinInRange(0.3...0.6, offset: 0.339, timeScale: 0.784, t: maskTimer), sinInRange(1.0...1.2, offset: 1.22, timeScale: 0.772, t: maskTimer)],
             [sinInRange(1.0...1.5, offset: 0.939, timeScale: 0.056, t: maskTimer), sinInRange(1.3...1.7, offset: 0.47, timeScale: 0.342, t: maskTimer)]
         ], colors: [
-            .red, .purple, .indigo,
+            .green, .purple, .indigo,
             .orange, .red, .blue,
             .yellow, .green, .mint
         ])
