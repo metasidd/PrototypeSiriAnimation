a2a19701c1a7a25c97a89c4cf496c85f0f12dc31
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-06-13 13:55:51 +0530

    Initial Commit

---
+++ SiriAnimationPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SiriAnimationPrototype
+//
+//  Created by Siddhant Mehta on 2024-06-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SiriAnimationPrototype/SiriAnimationPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriAnimationPrototypeApp.swift
+//  SiriAnimationPrototype
+//
+//  Created by Siddhant Mehta on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct SiriAnimationPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
